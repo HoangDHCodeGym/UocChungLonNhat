d66e2072841ac428d8ec046769c53fccbf7eb743
Hoang Dao <huuhoang.dao@gmail.com>
2018-10-07 09:30:30 +0700

    My first commit

--- src/GreatestCommonFactor.java
+++ src/GreatestCommonFactor.java
@@ -1,7 +1,29 @@
 import java.util.Scanner;
 
 public class GreatestCommonFactor {
-    Scanner scanner = new Scanner(System.in);
-    int a;
-    int b;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("Input a: ");
+        a = scanner.nextInt();
+        System.out.println("Input b: ");
+        b = scanner.nextInt();
+        System.out.println(GreatestCommonFactor.check(a,b));
+    }
+    public static String check(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            return "There is no common factor";
+        }
+        while (a != b) {
+            if (a > b) {
+                a = a-b;
+            } else {
+                b = b-a;
+            }
+        }
+        return "The greatest common factor is " + a;
+    }
 }
